04c5a76fe364ad7a0a7510ac01ca9014beacd233
marcelo <mgsouseu@gmail.com>
2020-03-21 22:46:33 -0300

    correcao de nomecaltura errada
    correcao de materais => materia no modal de cadastro

--- caderno-virtual/src/components/ModalCadastrar.tsx
+++ caderno-virtual/src/components/ModalCadastrar.tsx
@@ -25,7 +25,7 @@ const ModalCadastrar: React.FC = () =>{
             >
                 <Input
                     onChangeText={e => teste = e}                    
-                    placeholder='Matérias'
+                    placeholder='Matéria'
                     autoCapitalize="words"
                     leftIcon={
                         <Entypo 
