cc9fe512593ff03ad3bb6cfbe5e9e599dffac83b
Frederico <layeph@gmail.com>
2020-04-19 21:58:14 -0300

    Conserto Flash e Câmera
    ler titulo

--- caderno-virtual/src/pages/Camera/styles.ts
+++ caderno-virtual/src/pages/Camera/styles.ts
@@ -1,4 +1,4 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export const sCamera = StyleSheet.create({
     content: {
@@ -11,18 +11,8 @@ export const sCamera = StyleSheet.create({
         backgroundColor: 'transparent',
         flexDirection: 'row',
     },
-    
-    flipButton: {
-        alignSelf: 'flex-end',
-        alignItems: 'center',
-    },
-    flipText:{
-        fontSize: 18, 
-        marginBottom: 10, 
-        color: 'white'
-    },
 
-    zoomWrapper:{
+    zoomWrapper: {
         position: 'absolute',
         bottom: '50%',
         right: 15,
@@ -30,20 +20,38 @@ export const sCamera = StyleSheet.create({
         justifyContent: 'space-between',
         height: 90,
     },
+
+    cameraBottomWrapper: {
+        position: 'absolute',
+        bottom: 0,
+        right: 0,
+        left: 0,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        paddingBottom: 20,
+    },
+
+    cameraButton: {
+        height: 55,
+        width: 55,
+        borderRadius: 20,
+        backgroundColor: '#e8e8e8',
+        alignItems: 'center',
+        justifyContent: 'center',
+
+    },
+
+    flashButton: {
+        position: 'absolute',
+        left: 25,
+        bottom: 60,
+    },
+
+    flipButton: {
+        position: 'absolute',
+        left: 20,
+        bottom: 15,
+    },
+
 });
-
-
-
-
-// zoomInText: {
-//     fontSize: 18,
-//         marginBottom: 10,
-//             color: 'white',
-    
-//     },
-
-// zoomOutText: {
-//     fontSize: 25,
-//         marginBottom: 10,
-//             color: 'white',
-//     },
